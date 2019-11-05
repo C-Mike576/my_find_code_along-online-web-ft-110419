79a81b9ed491757a44ed912c960b6c5f166bf710
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 19:21:32 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,15 @@
 require 'pry'
-
+=begin
 def my_find(collection)
   i = 0
   while i < collection.length 
     return collection[i] if yield(collection[i])
     i += 1 
   end
+=end
+def my_find(collection)
+  for elmt in collection
+   return collection[elmt-1] if yield(collection[elmt-1])
+  end
+  nil
 end
